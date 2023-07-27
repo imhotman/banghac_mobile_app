a01a2b39096f764ee71bcf534eb3857b4328d6c7
hyeonjin <kimguswls23@gmail.com>
2023-07-27 14:03:29 +0900

    Link Main and Signup

--- app/src/main/java/com/example/banghac_mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/banghac_mobile_app/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.banghac_mobile_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.banghac_mobile_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUpTxt.setOnClickListener {
+                val intent = Intent(this, SignupActivity::class.java)
+                startActivity(intent)
+        }
     }
 }
